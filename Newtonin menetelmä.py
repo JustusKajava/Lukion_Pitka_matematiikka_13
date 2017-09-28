ac6d77bf077a4cb1308d2d029a399b27118fcb3c
JustusKajava <justuskajava@gmail.com>
2017-09-28 18:15:32 +0300

    Newtonin menetelmä nollakohtien etsimiseksi
    Lisäsin Newtonin menetelmä nollakohtien etsimiseksi

---
+++ Newtonin menetelmä.py
@@ -0,0 +1,11 @@
+#Newtonin menetelmä nollakohdan etsimiseen
+
+def f(x):#f(x) on tutkittava yhtälö, jolle etsitään nollakohtaa
+	return x**2
+def Df(x):#Df(x) on f'(x), eli f(x):n derivaatta
+	return 2x
+#Kysytään ohjelman ajajalta arvausta
+x0 = float(input("Anna arvaus nollakohdalle > "))
+for i in range(10):
+	x0 = x0 - f(x0)/Df(x0)
+	print("x"str(i) +" = "+ str(x0))
