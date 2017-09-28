1fb6f5a8060ac4d90f3860a89a0755e31c7827d8
JustusKajava <justuskajava@gmail.com>
2017-09-28 18:30:36 +0300

    Update Newtonin menetelmä.py

--- Newtonin menetelmä.py
+++ Newtonin menetelmä.py
@@ -3,7 +3,7 @@
 def f(x):#f(x) on tutkittava yhtälö, jolle etsitään nollakohtaa
 	return x**2
 def Df(x):#Df(x) on f'(x), eli f(x):n derivaatta
-	return 2x
+	return 2*x
 #Kysytään ohjelman ajajalta arvausta
 x0 = float(input("Anna arvaus nollakohdalle > "))
 for i in range(10):
