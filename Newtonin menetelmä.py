a4effbf8ee5022a59e88654358378c9e3bcffeb0
JustusKajava <justuskajava@gmail.com>
2017-09-28 18:41:33 +0300

    small edit

--- Newtonin menetelmä.py
+++ Newtonin menetelmä.py
@@ -6,6 +6,8 @@ def Df(x):#Df(x) on f'(x), eli f(x):n derivaatta
 	return 2*x
 #Kysytään ohjelman ajajalta arvausta
 x0 = float(input("Anna arvaus nollakohdalle > "))
-for i in range(10):
-	x0 = x0 - f(x0)/Df(x0)
+for i in range(40):
+	if(x0 == x0 - f(x0)/Df(x0)):
+		break
+	x0 = x0 - f(x0)/Df(x0
 	print("x" + str(i) + " = " + str(x0))
